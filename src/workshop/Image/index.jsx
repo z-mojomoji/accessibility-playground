17f1637bb03e330b58c162da28ca0f4aa43a7c06
Suphicha Anusirikul <suphicha.anusirikul@agoda.com>
2018-07-20 16:25:04 +0700

    remove an unnessary link

--- src/workshop/Image/index.jsx
+++ src/workshop/Image/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Image.scss';
 
 const Image = () => (
-    <img src="//pix6.agoda.net/hotelImages/514/51463/51463_16031811170040875839.jpg?s=1024x768" alt="" />
+    <img src="//pix6.agoda.net/hotelImages/514/51463/51463_16031811170040875839.jpg" alt="" />
 );
 
 export default Image;
